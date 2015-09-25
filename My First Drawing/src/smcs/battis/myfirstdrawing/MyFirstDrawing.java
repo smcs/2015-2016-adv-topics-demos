d0b8117313e29fdce5454b5161f5deb9ff36bee9
Seth Battis <sethbattis@stmarksschool.org>
2015-09-25 10:06:38 -0400

    My First Drawing
    Added ObjectDraw 1.1.1 as a User Library and created a first project `My First Drawing` using the library.

---
+++ My First Drawing/src/smcs/battis/myfirstdrawing/MyFirstDrawing.java
@@ -0,0 +1,11 @@
+package smcs.battis.myfirstdrawing;
+
+import objectdraw.*;
+
+public class MyFirstDrawing extends WindowController {
+	
+	public void begin() {
+		new FilledRect(20, 40, 100, 50, canvas);
+	}
+
+}
