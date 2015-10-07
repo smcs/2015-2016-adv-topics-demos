4bf6c102af884d0f6cbfe82982c423b896c738a9
Seth Battis <sethbattis@stmarksschool.org>
2015-10-07 10:54:03 -0400

    If structure comparison

--- 3. Dicey/src/smcs/battis/Dicey/Dicey.java
+++ 3. Dicey/src/smcs/battis/Dicey/Dicey.java
@@ -5,12 +5,43 @@ import objectdraw.*;
 public class Dicey extends WindowController {
 
 		private Text result;
+		private RandomIntGenerator generator;
 	
 		public void begin() {
 			result = new Text("Hello world", 10, 10, canvas);
+			
+			generator = new RandomIntGenerator(1, 6);
 		}
 		
 		public void onMouseClick(Location click) {
-			result.setText("(" + (int) click.getX() + ", " + (int) click.getY() + ")");
+			result.setText(generator.nextValue());
+			
+			// I want to check conditions in order, and not check anything
+			// after I find a true condition...
+			if (condition1) {
+				// only happens if condition1 is true
+			} else if (condition2) {
+				// only happens if condition1 is false and condition2 is true
+			} else {
+				// only happens if both condition1 and condition2 are false
+			}
+			
+			// uncontracted (but syntactically identical) form of above if structure
+			if (condition1) {
+				
+			} else {
+				if (condition2) {
+					
+				} else {
+					
+				}
+			}
 		}
 }
+
+
+
+
+
+
+
